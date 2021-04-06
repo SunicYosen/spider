7974dd37a4becbce32540373b6ab3b5e104f8bf2
sunicyosen <sunicyosen@outlook.com>
2021-04-06 20:00:46 +0800

    Add go to next page function

--- auto-study/weekly_exam.py
+++ auto-study/weekly_exam.py
@@ -14,13 +14,28 @@ def get_weekly_list_page(driver):
         exit()
 
 def get_weekly_last(driver):
-    weekly_list = driver.find_elements_by_tag_name("button")
+    weekly_got  = False
 
-    for week in weekly_list:
-        if week.get_attribute("innerText") == "开始答题":
-            week.click()
-            time.sleep(1)
-            break
+    while not weekly_got:
+        weekly_list = driver.find_elements_by_tag_name("button")
+
+        for week in weekly_list:
+            if week.get_attribute("innerText") == "开始答题":
+                driver.execute_script("arguments[0].scrollIntoView();", week)
+                time.sleep(0.5)
+                week.click()
+                weekly_got = True
+                time.sleep(0.5)
+                break
+
+        if not weekly_got:
+            next_page_button = driver.find_element_by_class_name(' ant-pagination-next')
+            if next_page_button:
+                driver.execute_script("arguments[0].scrollIntoView();", next_page_button)
+                next_page_button.click()
+                time.sleep(0.5)
+            else:
+                print("[-]: 没有找到可用的本周答题！")
 
 def weekly_exam(driver, url='https://pc.xuexi.cn/points/exam-index.html', questions_per_week=5):
     print("[+]: Do Weekly Exam - ", questions_per_week, "Question(s) per Week.")
